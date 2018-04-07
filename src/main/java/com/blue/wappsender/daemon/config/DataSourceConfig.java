4958d457972ae2b1603309c94a457f7f3c4a5030
mendozaju <jmendoza@despegar.com>
2018-04-07 11:45:14 -0300

    Se modifica cadena de conexion

--- src/main/java/com/blue/wappsender/daemon/config/DataSourceConfig.java
+++ src/main/java/com/blue/wappsender/daemon/config/DataSourceConfig.java
@@ -13,16 +13,21 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class DataSourceConfig {
+	//TODO: Sacar esta configuracion con properties
 	
 	@Bean(name="dataSource")
 	public DataSource getDataSource() {
-		return DataSourceBuilder
+		DataSource dataSource = DataSourceBuilder
 				.create()
-				.url("jdbc:mysql://localhost:3306/blue_wapp")
-				.username("root")
-				.password("root")
+				.url("jdbc:mysql://tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/r64ltc8bpwfhx9vz")
+				.username("cdalecwem3nxij68")
+				.password("lajfbi7nynpakgfi")
 				.driverClassName("com.mysql.jdbc.Driver")
 				.build();
+		//TODO: Buscar una manera mejor de confiruar la cantida maxima de conexiones
+		org.apache.tomcat.jdbc.pool.DataSource ds = (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
+		ds.setMaxActive(5);
+		return ds;
 	}
 	
 }
